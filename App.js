5bd628ebd5898ff8e186f3c1cc38a325c36ee469
newt0 <py_lambda@yahoo.co.jp>
2020-12-16 15:03:52 +0900

    add: get and display the articles from news api

--- App.js
+++ App.js
@@ -3,16 +3,25 @@ import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem';
 import dummyArticles from './dummies/article.json';
 import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
   const [articles, setArticles] = useState([]);
 
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    alert(Constants.manifest.extra.newsApiKey);
-    const timer = setTimeout(() => {
-      setArticles(dummyArticles);
-    }, 2000);
-    return () => clearTimeout(timer);
+    fetchArticles();
   }, []);
 
   return (
--- components/ListItem.js
+++ components/ListItem.js
@@ -5,12 +5,14 @@ const ListItem = ({ title, author, imageUrl }) => {
   return (
     <View style={styles.itemContainer}>
       <View style={styles.leftContainer}>
-        <Image
-          style={{ width: 100, height: 100 }}
-          source={{
-            uri: imageUrl,
-          }}
-        />
+        {!!imageUrl && (
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{
+              uri: imageUrl,
+            }}
+          />
+        )}
       </View>
       <View style={styles.rightContainer}>
         <Text style={styles.text} numberOfLines={3}>
