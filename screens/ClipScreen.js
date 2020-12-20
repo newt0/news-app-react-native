8278eaa15ca21c81cc657571e80ebb2e6d8b4f39
newt0 <py_lambda@yahoo.co.jp>
2020-12-20 12:07:15 +0900

    fix: {navigation}

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { StyleSheet, SafeAreaView, Text, FlatList } from 'react-native';
 import ListItem from '../components/ListItem';
 
-const ClipScreen = () => {
+const ClipScreen = ({ navigation }) => {
   const user = useSelector((state) => state.user);
   const { clips } = user;
   return (
